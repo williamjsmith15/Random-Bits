c9a6b6e648b6320b8db8e4826874c758caf13282
William Smith <williamjsmith15@hotmail.co.uk>
2021-10-27 15:20:13 +0100

    Add Newton to Celsius conversion

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -1,4 +1,6 @@
 %TEMPERATURE_CONDITIONS
+%Check temperature conversions between Kelvin, Fahrenhiet, Celsius and
+%Newton
 
 %Save degree symbol as variable
 deg = char(176);
@@ -7,3 +9,5 @@ disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(1
 fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
 fprint('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
 fprintf('Absolute zero is %g%sF.\n', celsius_to_fahrenheit(kelvin_to_celsius(0)), deg) 
+
+fprintf('33 %sN is %g%sC.\n', deg, newton_to_celsius(33), deg)
