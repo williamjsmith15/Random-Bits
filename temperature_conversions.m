2b174d30e82a3efb191a600a938e592ace914dc0
William Smith <williamjsmith15@hotmail.co.uk>
2021-10-27 15:36:20 +0100

    Comment key conversions section

--- temperature_conversions.m
+++ temperature_conversions.m
@@ -5,6 +5,7 @@
 %Save degree symbol as variable
 deg = char(176);
 
+%Check some key converions in this version
 disp(['Water boils at 100', deg, 'C, which is ', num2str(celsius_to_fahrenheit(100)), deg, 'F.'])
 fprintf('Water freezes at %g%sC, which is %g%sF.\n', fahrenheit_to_celsius(32), deg, 32, deg)
 fprint('Absolute zero is 0K, which is %g%sC.\n', kelvin_to_celsius(0), deg)
